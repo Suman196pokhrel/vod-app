b1ca1c0dcf4167e52b0b37092df58102f3de8dbf
Suman Pokhrel <spokhrel196@gmail.com>
2025-11-01 19:17:05 +1030

    chore: minor style changes

--- app/app/(public)/page.tsx
+++ app/app/(public)/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center  font-sans">
       <div>
         <h2 className="text-7xl font-extrabold">LANDING PAGE MATE</h2>
       </div>
