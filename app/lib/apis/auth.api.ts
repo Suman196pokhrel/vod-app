cb42139748cd30581cb6d39c0f8c7d7b3d58f7af
Suman Pokhrel <spokhrel196@gmail.com>
2025-11-09 23:34:12 +1030

    feat: added all auth endpoints api calls

--- app/lib/apis/auth.api.ts
+++ app/lib/apis/auth.api.ts
@@ -1,6 +1,8 @@
 import api from "./client";
 
 export const authAPI = {
+
+
   // signup
   signup: async (email: string, username: string, password: string) => {
     const response = await api.post("/auth/signup", {
@@ -27,6 +29,49 @@ export const authAPI = {
       refresh_token: refreshToken,
     });
   },
+
+  // Refresh
+  refresh: async (refreshToken:string)=>{
+    const response = await api.post("/auth/refresh",{
+        refresh_token:refreshToken,
+    });
+    return response.data
+  },
+
+  // verify-email
+  verifyEmail:async(token:string)=>{
+    const response = await api.get(`/auth/verify-email?token=${token}`)
+    return response.data
+  },
+
+  // Resend Verification
+  resendVerification: async(email:string)=>{
+    const response = await api.post("/auth/resend-verification",{
+        email:email
+    });
+    return response.data
+  },
+
+
+  // forgot pw
+  forgotPassword: async(email:string)=>{
+    const response = await api.post("/auth/forgot-password",{
+        email:email
+    });
+    return response.data
+  },
+
+  // Reset password
+  resetPassword: async(email:string, code:string, newPassword:string)=>{
+    const response = await api.post("/auth/reset-password",{
+        email:email,
+        code:code,
+        new_password: newPassword
+    })
+
+    return response.data
+  }
+
 
 
 };
