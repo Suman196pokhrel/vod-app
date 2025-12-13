9fd28e0f3f47d2d54d71e4c83832a0c8818804ab
Suman Pokhrel <spokhrel196@gmail.com>
2025-12-13 21:35:23 +1030

    updated video models with processing columsn

--- backend/app/models/videos.py
+++ backend/app/models/videos.py
@@ -17,12 +17,12 @@ class Video(Base):
     description = Column(String(5000), nullable=True)
     category = Column(String(50), index=True, nullable=False)  # NEW: action, drama, comedy, etc.
     
-    # File URLs (MinIO paths)
-    video_url = Column(String(500), nullable=False)
+    # MinIo paths for raw upload 
+    raw_video_path = Column(String(500), nullable=False)
     thumbnail_url = Column(String(500), nullable=True)
     
     # Video Metadata
-    duration = Column(Integer, nullable=True)  # Duration in minutes
+    # will extract this from video itself , duration = Column(Integer, nullable=True)  # Duration in minutes
     age_rating = Column(String(10), nullable=True)  # NEW: G, PG, PG-13, R, TV-14, TV-MA
     release_date = Column(Date, nullable=True)  # NEW: Release date
     
@@ -40,10 +40,29 @@ class Video(Base):
     # Publishing
     is_public = Column(Boolean, default=True)
     status = Column(String(20), default="draft")  # draft, published, scheduled
+
+    # processing status
+    processing_status = Column(String(30), default="uploading", index=True)
+    # Values : uploading , uploaded, processing , completed, failed
+
+    # Processing medata - stores infrom from prepare_video celery task
+    processing_metadata = Column(JSON, nullable=True)
+    # Will store: {"duration_seconds": 3600, "width": 1920, "height": 1080, "codec": "h264", "bitrate": 5000000}
+
+
+    # Error tracking
+    processing_error = Column(String(1000), nullable=True)
+    # Stores error message if processing_status = "failed"
+
     
     # Timestamps
     created_at = Column(DateTime(timezone=True), server_default=func.now(), index=True)
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    # ADD: For HLS streaming (filled after processing completes)
+    manifest_url = Column(String(500), nullable=True)  # Path to master.m3u8
+    available_qualities = Column(JSON, nullable=True)  # ["1080p", "720p", "480p", "360p"]
+
 
     # Foreign key
     user_id = Column(String(100), ForeignKey("users.id"), nullable=False, index=True)
