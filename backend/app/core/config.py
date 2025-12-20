2693ce962fd2e22b5960cdf668c7cffcce202fe7
Suman Pokhrel <spokhrel196@gmail.com>
2025-12-20 18:16:18 +1030

    added 144p transcoding

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,11 +9,12 @@ class Settings(BaseSettings):
     # video processing settings
     base_dir:str = str(Path(__file__).resolve().parent.parent.parent)
     processing_temp_dir: str = base_dir+ "/tmp" + "/video_processing"
-    QUALITY_SETTINGS = {
+    QUALITY_SETTINGS:dict = {
         "1080p": {"width": 1920, "height": 1080, "bitrate": "5000k"},
         "720p": {"width": 1280, "height": 720, "bitrate": "2500k"},
         "480p": {"width": 854, "height": 480, "bitrate": "1000k"},
-        "360p": {"width": 640, "height": 360, "bitrate": "500k"}
+        "360p": {"width": 640, "height": 360, "bitrate": "500k"},
+        "144p": {"width": 256, "height": 144, "bitrate": "200k"}
     }
 
 
