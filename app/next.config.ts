7b3135c09a74be263be0843e0b8dc663d9e1063a
Suman Pokhrel <spokhrel196@gmail.com>
2026-07-17 02:06:45 +0930

    added gatekeeping based image config for dev

--- app/next.config.ts
+++ app/next.config.ts
@@ -3,13 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    dangerouslyAllowLocalIP: process.env.NODE_ENV === "development",
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
+      { protocol: "http", hostname: "localhost", pathname: "/storage/**" },
     ],
   },
 };
